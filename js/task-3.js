ad8069b75b76a635db1726840d585ec61195ea0d
nksenechkav <144259943+nksenechkav@users.noreply.github.com>
2023-12-11 17:06:24 +0100

    add task-3

---
+++ js/task-3.js
@@ -0,0 +1,24 @@
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+};
+
+profile.changeUsername = function(newName) {
+    this.username = newName;
+};
+
+profile.updatePlayTime = function(hours) {
+    this.playTime += hours;
+};
+
+profile.getInfo = function() {
+    return this.username + " has " + this.playTime + " active hours!";
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
